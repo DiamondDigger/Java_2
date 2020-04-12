487155cde572c93e0face1247c143145c2a3ca16
naglec13 <naglec13@gmail.com>
2020-04-12 20:16:06 +0300

    ..

--- src/Demo_String/EqualString.java
+++ src/Demo_String/EqualString.java
@@ -8,10 +8,10 @@ public class EqualString {
         System.out.println("str1.equals(str2) = "+ str1.equals(str2));
         System.out.println("str1==str2 = "+ (str1==str2));
 
-        /*
-         */
+// String.compareTo
 
         String[] arrStr = {"new", "cat", "Bully","coin", "book", "wood"};
+
         for (int i = 0; i < arrStr.length ; i++) {
             for (int j = i+1; j < arrStr.length; j++) {
                 if (arrStr[j].compareTo(arrStr[i])<0) {
@@ -22,6 +22,22 @@ public class EqualString {
             }
         }
 
+        System.out.println("\nString.compareTo:");
+        for (String s : arrStr) {
+            System.out.println(s);
+        }
+
+        for (int i = 0; i < arrStr.length ; i++) {
+            for (int j = i+1; j < arrStr.length; j++) {
+                if (arrStr[j].compareToIgnoreCase(arrStr[i])<0) {
+                    String temp = arrStr[j];
+                    arrStr[j]=arrStr[i];
+                    arrStr[i]=temp;
+                }
+            }
+        }
+
+        System.out.println("\nString.compareToIgnoreCase:");
         for (String s : arrStr) {
             System.out.println(s);
         }
