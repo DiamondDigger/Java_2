5a1a38dc3b404a60a6ca575238b2cc0c2107dd09
naglec13 <naglec13@gmail.com>
2020-04-13 21:57:41 +0300

    ..

--- src/Demo_String/EqualString.java
+++ src/Demo_String/EqualString.java
@@ -58,7 +58,8 @@ public class EqualString {
         stringBuffer.append("Java");
         System.out.println("stringBuffer.equals(stringBuilder): " + (stringBuffer.equals(stringBuilder)));
         System.out.println("stringBuffer.hashCode()==stringBuilder.hashCode(): " + (stringBuffer.hashCode() == stringBuilder.hashCode()));
-        System.out.println("stringBuffer.toString().hashCode() == stringBuilder.toString().hashCode(): "+(stringBuffer.toString().hashCode() == stringBuilder.toString().hashCode()));
+        System.out.println("stringBuffer.toString().hashCode() == stringBuilder.toString().hashCode(): " + (stringBuffer.toString().hashCode() == stringBuilder.toString().hashCode()));
+        System.out.println("stringBuffer.toString() == stringBuilder.toString(): " + (stringBuffer.toString() == stringBuilder.toString()));
 
     }
 }
